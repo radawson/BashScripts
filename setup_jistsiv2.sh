15607fcf6a3eb7c550ceea429d2f9c0de67fdeaf
Rick Dawson <dawsora@gmail.com>
2025-03-27 19:23:21 -0400

    fix: Add WebSocket support and update proxy settings in Nginx configuration

--- setup_jistsiv2.sh
+++ setup_jistsiv2.sh
@@ -231,6 +231,11 @@ server {
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_http_version 1.1;
+        
+        # Add WebSocket support
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
     }
 }
 EOF
